2ed68f58be3c73415d2b4587814afe76cef8784e
StrawRose502416 <josegabrielvillanerajaimes@gmail.com>
2026-06-03 03:17:56 +0000

    local administrator

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-const CACHE = "gestor-v1";
-const FILES = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE = "gestor-v2";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
+// Instala y guarda TODO en caché desde el primer uso
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
   self.skipWaiting();
 });
 
+// Elimina cachés viejos
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -15,8 +25,16 @@ self.addEventListener("activate", e => {
   clients.claim();
 });
 
+// Cache-first: siempre sirve desde caché, nunca necesita internet
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match("./index.html"));
+    })
   );
 });
